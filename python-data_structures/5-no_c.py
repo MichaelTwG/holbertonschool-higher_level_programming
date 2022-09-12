dd7df070e107db3487c588fee5e9dba41c02d1f7
MichaelTwG <michaelgolenia1@gmail.com>
2022-09-12 08:26:37 -0700

    HELP

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    cp_list = []
+    lst = []
     for char in my_string:
-        cp_list.append(char)
-    for i in range(0, len(cp_list)):
-        if cp_list[i] == 'c' or cp_list[i] == 'C':
-            cp_list[i] == ""
-    return "".join(cp_list)
+        lst.append(char)
+    for i in range(0, len(lst)):
+        if lst[i] == 'C' or lst[i] == 'c':
+            lst[i] == ''
+    return ''.join(lst)
