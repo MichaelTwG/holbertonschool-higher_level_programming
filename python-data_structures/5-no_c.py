29b1dc93a1422ed7de1d5b9e4a94c6125db80aa7
MichaelTwG <michaelgolenia1@gmail.com>
2022-09-12 08:12:35 -0700

    5-no_c.py

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    cp_list = []
+    for char in my_string:
+        cp_list.append(char)
+    for i in range(0, len(cp_list)):
+        if cp_list[i] == 'c' or cp_list[i] == 'C':
+            cp_list[i] == ""
+    return "".join(cp_list)
