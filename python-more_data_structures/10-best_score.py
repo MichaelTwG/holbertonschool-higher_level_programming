c5876651c15bb546cd0f29c5ece4d962007c2b0c
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-14 12:37:03 -0300

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from re import A
+
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    score = 0
+    for key, value in a_dictionary.items():
+        if value > score:
+            highest_score_key = key
+            score = value
+    return highest_score_key
