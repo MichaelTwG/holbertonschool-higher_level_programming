6a8cbd8fb141f994f659924eafcc70432968bbef
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-14 12:20:28 -0300

    9-multiply_by_2.py

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pyhon3
+def multiply_by_2(a_dictionary):
+    a_dictionary2 = a_dictionary.copy()
+    for key, value in a_dictionary2.items():
+        a_dictionary2[key] = value * 2
+    return a_dictionary2
