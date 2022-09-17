fbd2edd5e24424b6dbadb52857c760fdbdcbcd5b
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-17 08:02:29 -0300

    error of return value

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    int_list = []
     try:
         for i in range(0, x):
             print("{:d}".format(my_list[i]), end="")
-            int_list.append(my_list[i])
             count += 1
         print()
+        return count
     except (IndexError, ValueError, TypeError):
         pass
