86ec3b00499f75a639bbdc2096479a1791793293
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-17 07:57:56 -0300

    NameError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     int_list = []
     try:
         for i in range(0, x):
-            print("{:d}".format(mi_list[i]), end="")
-            int_list.append(mi_list[i])
+            print("{:d}".format(my_list[i]), end="")
+            int_list.append(my_list[i])
             count += 1
         print()
     except (IndexError, ValueError, TypeError):
