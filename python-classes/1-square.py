94116c5394c5608d8575ea66e9621ab1fd9d58db
MichaelTwG <michaelgolenia1@gmail.com>
2022-09-20 17:02:46 -0300

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+"""
+    Create a class called Square
+"""
+
+
+class Square:
+    """ Atribute: size (int): private size of the square """
+
+    def __init__(self, size):
+        """ Instantiation Variables: Size """
+        self.__size = size
