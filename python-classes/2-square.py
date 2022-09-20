0e695bc2500e567c28227bb861397e55692264a9
MichaelTwG <michaelgolenia1@gmail.com>
2022-09-20 17:25:07 -0300

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Create a class called Square
+"""
+
+
+class Square:
+    """ Atribute: size (int): private size of the square """
+
+    def __init__(self, size=0):
+        """ Instantiation Variables: Size """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
