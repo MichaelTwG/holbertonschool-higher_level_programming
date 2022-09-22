340cef2e190afb470c777767386fe60983e73608
MichaelTwG <michaelgolenia1@gmail.com>
2022-09-22 11:47:46 -0300

    tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+    this function add two ints or floats
+"""
+
+
 def add_integer(a, b=98):
+    """add_integer add a and b"""
     if type(a) != float and type(a) != int:
         print("a must be an integer")
     if type(b) != float and type(b) != int:
