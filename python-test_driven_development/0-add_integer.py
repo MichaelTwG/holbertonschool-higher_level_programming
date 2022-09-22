b219e2489f4bcfeaa2a5d63b7d3db6bcb6f394db
MichaelTwG <michaelgolenia1@gmail.com>
2022-09-22 11:54:23 -0300

    tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,8 +8,11 @@ def add_integer(a, b=98):
     """add_integer add a and b"""
     if type(a) != float and type(a) != int:
         print("a must be an integer")
-        a = int(a)
     if type(b) != float and type(b) != int:
         print("b must be an integer")
+    if type(b) == float:
         b = int(b)
+    if type(a) == float:
+        a = int(a)
+    if a is not None and b is not None:
         return (a+b)
