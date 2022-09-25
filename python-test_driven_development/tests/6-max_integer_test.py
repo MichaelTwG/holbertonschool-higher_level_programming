c45c08bbffa92a4b9626a601229519a598c9043a
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-25 17:02:00 -0300

    test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -13,20 +13,23 @@ class Test_max_integer(unittest.TestCase):
         Unitests for max_integer
     """
 
-    def text_empty(self):
+    def test_empty(self):
         self.assertEqual(max_integer([]), None)
 
-    def text_max_end(self):
+    def test_max_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
 
-    def text_max_beggining(self):
+    def test_max_beggining(self):
         self.assertEqual(max_integer([5, 1, 2, 3, 4]), 5)
 
-    def text_negative_values(self):
+    def test_negative_values(self):
         self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
 
-    def text_negative_and_positive(self):
+    def test_negative_and_positive(self):
         self.assertEqual(max_integer([1, 3, -6, 5]), 5)
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([3]), 3)
 
 
 if __name__ == '__main__':
