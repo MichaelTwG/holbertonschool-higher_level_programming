b1f6422479adcffbb722f1657f0dc813c2a4a516
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-25 14:54:49 -0300

    output error

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,7 +9,7 @@ def print_square(size):
         this function print a square with the character #
     """
     if type(size) is not int:
-        raise TypeError("isize must be an integer")
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(0, size):
