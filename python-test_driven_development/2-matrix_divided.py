4bcf4319d2abb34ad8ee2b9a3048220a415dd6f6
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-25 12:57:58 -0300

    2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -20,8 +20,8 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for j in matrix[i]:
             if not isinstance(j, int) and not isinstance(j, float):
-                raise TypeError("matrix must be a matrix (list of list) \
-of integers/floats")
+                raise TypeError("matrix must be a matrix\
+ (list of lists) of integers/floats")
             row.append(round(j / div, 2))
         result.append(row)
         row = []
