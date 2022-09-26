ea9cffbbc2151e2e11cbc80603e452caf3892e1b
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-26 14:01:42 -0300

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module rectangle.py - create a class called Rectangle"""
+
+
+class Rectangle:
+    """
+        The clase Rectangle
+            Args:
+                Width (int)
+                Height (int)
+    """
+    pass
