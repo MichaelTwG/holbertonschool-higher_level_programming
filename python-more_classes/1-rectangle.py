0375999f8eac82d8bd61be44b332dd1f2625ba1e
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-26 14:12:21 -0300

    1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Module rectangle.py - create a class called Rectangle"""
+
+
+class Rectangle:
+    """
+        The clase Rectangle
+            Args:
+                Width (int)
+                Height (int)
+    """
+    def __init__(self, width=0, height=0):
+        """ Constructor method for the class Rectangle, this code is ejecuted
+        when an new istnace is created
+        """
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+
+        @property
+        def height(self):
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            elif value < 0:
+                raise ValueError("height must be >= 0")
