597b719e4ff71bff9cb75a2251980c50dec222ed
Michael Golenia <michaelgolenia1@gmail.com>
2022-09-30 15:08:33 -0300

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module create a new class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+        BaseGeometry class
+            Methods:
+                area ()
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
