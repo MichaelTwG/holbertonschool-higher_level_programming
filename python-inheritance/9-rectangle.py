1b36fc22c17b744a21ed6746b35c100bf97f3b74
Michael Golenia <michaelgolenia1@gmail.com>
2022-10-03 19:40:43 -0300

    9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Module Rectangle"""
+from curses.textpad import rectangle
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+        Class rectangle:
+            Width
+            Height
+        Methds:
+            area
+            print
+            str
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ return the aria of the rectangle """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] <{self.__width}>/<{self.__height}>"
