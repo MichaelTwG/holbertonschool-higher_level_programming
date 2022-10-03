c85143d1b8583fe6777c97f8aa15f6df4eeabe6f
Michael Golenia <michaelgolenia1@gmail.com>
2022-10-03 19:53:36 -0300

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module Square inherit from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        Class Rectangle
+            Args:   size
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
