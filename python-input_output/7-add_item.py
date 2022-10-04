771c174f49b91934a242717f386bffc0a170608c
Michael Golenia <michaelgolenia1@gmail.com>
2022-10-04 13:31:25 -0300

    permisions

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 '''Module 7-add_item'''
-
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-len_arg = len(sys.argv)
 JSON_list = []
 
-for arg in range(1, len_arg):
+for arg in range(1, len(sys.argv)):
     JSON_list.append(sys.argv[arg])
 try:
     JSON_list2 = load_from_json_file("add_item.json")
