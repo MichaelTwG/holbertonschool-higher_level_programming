027fc38d1ce35131ba7232c2fc74b8757c92871b
Michael Golenia <michaelgolenia1@gmail.com>
2022-10-04 11:00:45 -0300

    blankline

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ read_file and print in stdout"""
     with open(filename, encoding='utf-8') as File:
-        print(File.read())
+        print(File.read(), end="")
