73f90ff54fa59eab2c3f32f238d28ef357dbf37a
Michael Golenia <michaelgolenia1@gmail.com>
2022-10-04 12:49:47 -0300

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,6 +2,7 @@
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
+from typing import final
 
 """ CREATE A LIST TO CONVERT """
 len_arg = len(sys.argv)
@@ -9,5 +10,9 @@ JSON_list = []
 
 for arg in range(1, len_arg):
    JSON_list.append(sys.argv[arg])
-   
-save_to_json_file(JSON_list, "add_item.json")
+try:
+    JSON_list2 = load_from_json_file("add_item.json")
+except:
+    save_to_json_file([], "add_item.json")
+finally:
+    save_to_json_file(JSON_list, "add_item.json")
