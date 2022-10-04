9297091bd48ef42168c3b94a2d89234cc3d42bb2
Michael Golenia <michaelgolenia1@gmail.com>
2022-10-04 10:59:15 -0300

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module read_file"""
+
+
+def read_file(filename=""):
+    """ read_file and print in stdout"""
+    with open(filename, encoding='utf-8') as File:
+        print(File.read())
