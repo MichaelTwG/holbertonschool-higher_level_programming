65b71234c1967c7faa37d5bb09165d186244afbb
Michael Golenia <michaelgolenia1@gmail.com>
2022-10-04 13:48:06 -0300

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module Student"""
+
+
+from typing import Dict
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ constructor of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return a dict of the class"""
+        Mydict = vars(self)
+        Mydict2 = dict()
+        if attrs is None:
+            return Mydict
+        
+        for key, value in Mydict:
+            for key_att in attrs:
+                if key is key_att:
+                    Mydict2[key] = value
+        return Mydict2
