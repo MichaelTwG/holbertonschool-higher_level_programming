ce79fa053812bafae589b40177ffdaf0741a9f30
MichaelTwG <michaelgolenia1@gmail.com>
2022-11-04 11:26:09 -0700

    List the numbero of recors with the same score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- List the number of records with the same score in the table
+-- display the score and number
+-- the list should be sorted bby number of records DESC
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
