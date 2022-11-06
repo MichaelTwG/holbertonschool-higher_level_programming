c370df87cdb0e46cb99163716a0ed14a3b6bea42
MichaelTwG <michaelgolenia1@gmail.com>
2022-11-05 20:45:33 -0700

    SUBQUERIES

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- List all cities of Califonia that can be found in database hbtn_0d_usa
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states where name = 'California');
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
