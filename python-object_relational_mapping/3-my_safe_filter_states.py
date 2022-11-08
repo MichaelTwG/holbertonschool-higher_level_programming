f8684bf06c3a476ff3f347f078bc081170eef546
MichaelTwG <michaelgolenia1@gmail.com>
2022-11-08 08:32:42 -0800

    prevent SQL Injection | Task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+    Module 0-select_states.py
+"""
+import MySQLdb
+import sys
+
+
+class DataBase:
+    """ Create the class DataBase to connect with MySQL """
+
+    def __init__(self, user, passwd, bd, port, host):
+        """ The constructor of the class """
+        self.user = user
+        self.passwd = passwd
+        self.bd = bd
+        self.port = port
+        self.host = host
+
+    def DBConection(self):
+        """ Return the conection of the BD"""
+
+        return MySQLdb.connect(
+                                host=self.host,
+                                port=self.port,
+                                user=self.user,
+                                passwd=self.passwd,
+                                db=self.bd
+                              )
+
+    def Cursor(self):
+        """ Return the cursor of the db"""
+
+        return self.DBConection().cursor()
+
+
+if __name__ == '__main__':
+
+    conection_data = sys.argv
+
+    Uname = conection_data[1]
+    Upasswd = conection_data[2]
+    BDname = conection_data[3]
+
+    DataBase_1 = DataBase(Uname, Upasswd, BDname, 3306, "localhost")
+
+    DataBase_1_conection = DataBase_1.DBConection()
+    cursor = DataBase_1.Cursor()
+    name = conection_data[4].split(";")
+    consult = "SELECT * from states "
+    consult_2 = "WHERE BINARY name='{}'".format(name[0])
+    cursor.execute(consult + consult_2 + " ORDER BY id ASC")
+
+    states = cursor.fetchall()
+
+    for state in states:
+
+        print(state)
