d12cee925fcbd8d2c0f717c3059f979b7d6205d2
MichaelTwG <michaelgolenia1@gmail.com>
2022-11-10 13:30:54 -0800

    model state with SQLAlchemy

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Modulo state """
+"""
+    Modulo state
+"""
 
 from sqlalchemy.ext.declarative import delcarative_base
 from sqlalchemy import Column, Integer, String
@@ -8,8 +10,9 @@ Base = delcarative_base()
 
 
 class State(Base):
-    """ SQL Model State with SQLAlchemy """
-
+    """
+        SQL Model State with SQLAlchemy
+    """
     __tablename__ = "states"
 
     id = Column(Integer(), primary_key=True,
