d662a88e08408875a32a371843b4d727eca0b5ad
MichaelTwG <michaelgolenia1@gmail.com>
2022-11-10 13:27:11 -0800

    model state with SQLAlchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pyhon3
+""" Modulo state """
+
+from sqlalchemy.ext.declarative import delcarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = delcarative_base()
+
+
+class State(Base):
+    """ SQL Model State with SQLAlchemy """
+
+    __tablename__ = "states"
+
+    id = Column(Integer(), primary_key=True,
+                nullable=False, unique=True, autoincrement=True
+                )
+    name = Column(String(128), nullable=False)
