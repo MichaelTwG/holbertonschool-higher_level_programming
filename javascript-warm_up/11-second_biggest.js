f3d5959b4496daf111488255e2edddd6cc8137cb
Your Name <michaelgolenia1@gmail.com>
2022-12-06 15:48:02 -0300

    fixexed

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
 const numbers = [];
-if (args[2] === '1' || args[2] === NaN) {
-  console.log(1)
+if (args[2] === '1' && args.length < 4) {
+  console.log(1);
 } else if (args.length < 3) {
-  console.log('1');
+  console.log(1);
 } else {
   for (let i = 2; i < args.length; i += 1) {
     numbers.push(Number(args[i]));
