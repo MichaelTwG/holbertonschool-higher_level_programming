9714f8c1beaf538523af709beeb434194bb1a999
Your Name <michaelgolenia1@gmail.com>
2022-12-11 22:38:35 -0300

    6-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const sQ = require('./5-square');
+class Square extends (sQ) {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    let outStr = '';
+
+    for (let i = 0; i < this.height; i += 1) {
+      for (let j = 0; j < this.width; j += 1) {
+        if (c == 'C') {
+          outStr += 'C';
+        } else {
+          outStr += 'X';
+        }
+      }
+      if (i < this.height - 1) {
+        outStr += '\n';
+      }
+    }
+    console.log(outStr);
+  }
+}
+
+module.exports = Square;
